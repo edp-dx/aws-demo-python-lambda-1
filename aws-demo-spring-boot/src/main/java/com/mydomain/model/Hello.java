74db5d2351f759bd89e33c29f4d1582b0983be06
Andrey Trubitsyn <andtrub@gmail.com>
2024-10-21 16:10:36 +0200

    Create Hello model for Spring Boot application

---
+++ aws-demo-spring-boot/src/main/java/com/mydomain/model/Hello.java
@@ -0,0 +1,13 @@
+package com.mydomain.model;
+
+public class Hello {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
